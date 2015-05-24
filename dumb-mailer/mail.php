71f7eb90e878046bf7ef8104ea04eaf2707b1b9a
Midori Koçak <mtkocak@gmail.com>
2015-05-24 15:39:39 +0200

    credentials

--- dumb-mailer/mail.php
+++ dumb-mailer/mail.php
@@ -33,10 +33,10 @@ $mail = new PHPMailer;
 //$mail->SMTPDebug = 3;                               // Enable verbose debug output
 
 $mail->isSMTP();                                      // Set mailer to use SMTP
-$mail->Host = 'smtp.gmail.com';  // Specify main and backup SMTP servers
+$mail->Host = 'smtp.mandrillapp.com ';  // Specify main and backup SMTP servers
 $mail->SMTPAuth = true;                               // Enable SMTP authentication
 $mail->Username = 'mtkocak@gmail.com';                 // SMTP username
-$mail->Password = 'xmA1e#ST';                           // SMTP password
+$mail->Password = 'cxzCKql5JD1IAyTuSaHFEw';                           // SMTP password
 $mail->SMTPSecure = 'tls';                            // Enable TLS encryption, `ssl` also accepted
 $mail->Port = 587;                                    // TCP port to connect to
 
